28338ab33e7efc61590e75cca838c2a34d592f29
gsirbiladze <grigoli@gmail.com>
2017-10-14 11:13:57 -0700

    Update text_to_html_representation.py

--- text_to_html_representation.py
+++ text_to_html_representation.py
@@ -1,4 +1,11 @@
 
+
+
+def text_to_html_representation(text, hexformat=False):
+     return "".join(['&#%s;'%str(hex(ord(c)))[1:] for c in text]) if hexformat else "".join(['&#%s;'%str(ord(c)) for c in text])
+
+
+# text in bytes
 def text_to_html_representation(text, hexformat=False):
      return "".join(['&#%s;'%str(hex(ord(c)))[1:] for c in text.decode('utf-8')]) if hexformat else "".join(['&#%s;'%str(ord(c)) for c in text.decode('utf-8')])
 
