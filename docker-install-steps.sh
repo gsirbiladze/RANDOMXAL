62c006139a26c59219463c077daab2b58f27855f
gsirbiladze <grigoli@gmail.com>
2019-10-02 15:47:57 -0700

    Docker Is Awesome

--- docker-install-steps.sh
+++ docker-install-steps.sh
@@ -11,7 +11,6 @@ sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add 
 # apt-key fingerprint 0EBFCD88
 
 
-
 # For Mint find based on which Ubuntu release is it 
 #   * cat /etc/upstream-release/lsb-release
 # and sub "$(lsb_release -cs)" with that.
@@ -28,5 +27,16 @@ sudo apt-get update
 
 # Install Docker  CE(Community Edition)
 sudo apt-get install docker-ce
+
+
+# Docker composer and machine
+URL_DMB="https://github.com/docker/machine/releases/download/v0.16.0/docker-machine-$(uname -s)-$(uname -m)"
+URL_DCB="https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)"
+
+DMB="/usr/local/bin/docker-machine"
+DCB="/usr/local/bin/docker-compose"
+
+sudo curl -L "$URL_DMB" -o "$DMB" && sudo chmod +x "$DMB"
+sudo curl -L "$URL_DCB"  -o "$DCB" && sudo chmod +x "$DCB"
 
 
